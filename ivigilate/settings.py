5d0c34517bd98bb4f0f65646b30fb099eba76af0
Nuno Freire <nunofcf@gmail.com>
2015-10-25 15:05:22 +0000

    changed log location

--- ivigilate/settings.py
+++ ivigilate/settings.py
@@ -150,7 +150,7 @@ LOGGING = {
         'log_file':{
             'level': 'DEBUG',
             'class': 'logging.handlers.RotatingFileHandler',
-            'filename': os.path.join(BASE_DIR, 'logs/ivigilate.log'),
+            'filename': os.path.join(BASE_DIR, 'log/ivigilate.log'),
             'maxBytes': LOG_SIZE,
             'backupCount': 10,
             'formatter': 'verbose'
