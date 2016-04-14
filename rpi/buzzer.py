90e4b3b48917fe39da1fc6fd2ffa304f992aaf56
Andre Gloria <admg@ist.utl.pt>
2016-04-14 01:37:31 +0100

    Add buzzer module

---
+++ rpi/buzzer.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+# External module imports, making sure we have root superuser privileges
+import time
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
+
+
+# Definitions (user changeable)
+buzzerPin = 17 # Broadcom pin 17 (P1 pin 11)
+freq_l = 600 # tone low frequency
+freq_h = 800 # tone high frequency
+
+
+# Definitions
+dc = 50 # duty cycle (0-100) for PWM pin
+buzzer = None
+
+def init():
+	global buzzer
+	GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+	GPIO.setup(buzzerPin, GPIO.OUT) # Buzzer pin set as output
+	buzzer = GPIO.PWM(buzzerPin, freq_l)  # Initialize PWM on lower frequency tone
+
+# If no argument is passed to this function then it will play for 30 seconds
+def play(duration = 30):
+	try:
+		buzzer.start(dc)
+	except AttributeError:
+		print("Error: Buzzer not previously initialized! You must call init() before play().")
+		return
+		
+		
+	loops = duration // 0.5 # integer division
+
+	while loops > 0:
+		buzzer.ChangeFrequency(freq_l)
+		time.sleep(0.2)
+		buzzer.ChangeFrequency(freq_h)
+		time.sleep(0.2)
+		
+		loops -= 1
+
+	# Cleanup			
+	buzzer.stop() # stop PWM
+	GPIO.cleanup() # cleanup all used GPIOs
